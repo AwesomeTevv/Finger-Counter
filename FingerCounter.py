4949d9f7b946e4922dd9fa3056f8be53fa1a914f
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-01-29 10:59:33 +0200

    Added fps display

--- FingerCounter.py
+++ FingerCounter.py
@@ -10,8 +10,17 @@ cap = cv2.VideoCapture(0)
 cap.set(3, cam_width)
 cap.set(4, cam_height)
 
+prev_time = 0
+curr_time = 0
+
 while True:
     success, img = cap.read()
+    
+    curr_time = time.time()
+    fps = 1 / (curr_time - prev_time)
+    prev_time = curr_time
+    
+    cv2.putText(img, f'fps: {int(fps)}', (20, 40), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
     
     cv2.imshow("Finger Counter", img)
     cv2.waitKey(1)
