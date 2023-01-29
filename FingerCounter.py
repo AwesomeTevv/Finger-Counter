2a33bf14c3b0875af71cc6825fd5b749391a80ff
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-01-29 10:54:54 +0200

    Added webcam access

--- FingerCounter.py
+++ FingerCounter.py
@@ -2,3 +2,17 @@ import cv2
 import time
 import os
 
+cam_width = 1280
+cam_height = 720
+
+cap = cv2.VideoCapture(0)
+
+cap.set(3, cam_width)
+cap.set(4, cam_height)
+
+while True:
+    success, img = cap.read()
+    
+    cv2.imshow("Finger Counter", img)
+    cv2.waitKey(1)
+
